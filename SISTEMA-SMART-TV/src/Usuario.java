7345f5d9a4e613d674772e19b94792646376e90d
Calebeans <rcalebe655@gmail.com>
2024-04-25 13:27:10 -0300

    Smart Tv Java

---
+++ SISTEMA-SMART-TV/src/Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Volume: " + smartTv.volume);
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal? " + smartTv.canal);
+        System.out.println("Volume? " + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Novo Canal: " + smartTv.canal);
+
+    }
+}
